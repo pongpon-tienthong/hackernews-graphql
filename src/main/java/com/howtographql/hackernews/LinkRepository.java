7e4f581daac69acac4402e341033bda678289366
Pongpon Tienthong <pongpontienthong@Pongpons-MBP.fios-router.home>
2019-12-23 19:20:08 -0500

    Tutorial on GraphQL done

---
+++ src/main/java/com/howtographql/hackernews/LinkRepository.java
@@ -0,0 +1,54 @@
+package com.howtographql.hackernews;
+
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.mongodb.client.model.Filters.eq;
+
+public class LinkRepository {
+
+    private final MongoCollection<Document> links;
+
+    public LinkRepository(MongoCollection<Document> links) {
+        this.links = links;
+    }
+
+    public List<Link> getAllLinks() {
+        List<Link> allLinks = new ArrayList<>();
+        for (Document doc : links.find()) {
+            Link link = new Link(
+                    doc.get("_id").toString(),
+                    doc.getString("url"),
+                    doc.getString("description"),
+                    doc.getString("postedBy")
+            );
+            allLinks.add(link);
+        }
+        return allLinks;
+    }
+
+    public void saveLink(Link link) {
+        Document doc = new Document();
+        doc.append("url", link.getUrl());
+        doc.append("description", link.getDescription());
+        doc.append("postedBy", link.getUserId());
+        links.insertOne(doc);
+    }
+
+    public Link findById(String id) {
+        Document doc = links.find(eq("_id", new ObjectId(id))).first();
+        return link(doc);
+    }
+
+    private Link link(Document doc) {
+        return new Link(
+                doc.get("_id").toString(),
+                doc.getString("url"),
+                doc.getString("description"),
+                doc.getString("postedBy"));
+    }
+}
